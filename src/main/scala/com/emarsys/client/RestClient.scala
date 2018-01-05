6308d7b9641187129bda50a2b1cdac22dba6ec12
Margit Fawal <margit.fawal@emarsys.com>
2018-01-05 14:58:22 +0100

    [extension] extends RestClient with function runRawE which return either instead of exception in version 0.1.9

--- src/main/scala/com/emarsys/client/RestClient.scala
+++ src/main/scala/com/emarsys/client/RestClient.scala
@@ -11,9 +11,9 @@ import com.emarsys.escher.akka.http.EscherDirectives
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
 trait RestClient extends EscherDirectives {
-  implicit val system:       ActorSystem
+  implicit val system: ActorSystem
   implicit val materializer: Materializer
-  implicit val executor:     ExecutionContextExecutor
+  implicit val executor: ExecutionContextExecutor
 
   val connectionFlow: Flow[HttpRequest, HttpResponse, _]
   val serviceName: String
@@ -25,18 +25,32 @@ trait RestClient extends EscherDirectives {
 
   def runRaw[S](request: HttpRequest, retry: Int = maxRetryCount)(implicit um: Unmarshaller[ResponseEntity, S]): Future[S] = {
     for {
-      signed   <- signRequest(serviceName)(executor, materializer)(request)
+      result <- runRawE[S](request, retry).map {
+        case Left((status, responseBody)) => {
+          system.log.error("Request to {} failed with status: {} / body: {}", request.uri, status, responseBody)
+          throw RestClientException(s"Rest client request failed for ${request.uri}", status, responseBody)
+        }
+        case Right(response) => response
+      }
+    } yield result
+  }
+
+  def runRawE[S](request: HttpRequest, retry: Int = maxRetryCount)(implicit um: Unmarshaller[ResponseEntity, S]): Future[Either[(Int, String), S]] = {
+    for {
+      signed <- signRequest(serviceName)(executor, materializer)(request)
       response <- sendRequest(signed)
-      result   <- response.status match {
-              case Success(_) => Unmarshal(response.entity).to[S]
-              case ServerError(_) if retry > 0 =>
-                system.log.info("Retrying request: {} / {} attempt(s) left", request.uri, retry - 1)
-                runRaw[S](request, retry - 1)
-              case status => Unmarshal(response.entity).to[String].map { responseBody =>
-                system.log.error("Request to {} failed with status: {} / body: {}", request.uri, status, responseBody)
-                throw RestClientException(s"Rest client request failed for ${request.uri}", status.intValue, responseBody)
-              }
-            }
-      } yield result
-    }
+      result <- response.status match {
+        case Success(_) => Unmarshal(response.entity).to[S].map(Right(_))
+        case ServerError(_) if retry > 0 =>
+          system.log.info("Retrying request: {} / {} attempt(s) left", request.uri, retry - 1)
+          runRawE[S](request, retry - 1)
+        case status => Unmarshal(response.entity).to[String].map { responseBody =>
+          system.log.error("Request to {} failed with status: {} / body: {}", request.uri, status, responseBody)
+          Left((status.intValue, responseBody))
+        }
+      }
+    } yield result
+  }
+
+
 }
